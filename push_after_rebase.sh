85b15925016088dd945ace26fab98c5d453a8381
DroidFreak32 <rushabshah32@gmail.com>
2017-09-21 23:28:23 +0530

    Create push_after_rebase.sh

---
+++ push_after_rebase.sh
@@ -0,0 +1,110 @@
+#!/bin/bash
+
+cd bionic
+git push origin oreo backup --force
+cd ..
+
+
+cd bootable_recovery
+git push origin oreo backup --force
+cd ..
+
+
+cd external_selinux
+git push origin oreo backup --force
+cd ..
+
+
+cd frameworks_av
+git push origin oreo backup --force
+cd ..
+
+
+cd frameworks_base
+git push origin oreo backup --force
+cd ..
+
+
+cd frameworks_native
+git push origin oreo backup --force
+cd ..
+
+
+cd hardware_interfaces
+git push origin oreo backup --force
+cd ..
+
+
+cd hardware_libhardware
+git push origin oreo backup --force
+cd ..
+
+
+cd hardware_libhardware_legacy
+git push origin oreo backup --force
+cd ..
+
+
+cd hardware_qcom_gps
+git push origin oreo backup --force
+cd ..
+
+
+cd hardware_qcom_wlan
+git push origin oreo backup --force
+cd ..
+
+
+cd packages_apps_Camera2
+git push origin oreo backup --force
+cd ..
+
+
+cd packages_apps_Gallery2
+git push origin oreo backup --force
+cd ..
+
+
+cd packages_apps_Settings
+git push origin oreo backup --force
+cd ..
+
+
+cd packages_inputmethods_LatinIME
+git push origin oreo backup --force
+cd ..
+
+
+cd packages_providers_MediaProvider
+git push origin oreo backup --force
+cd ..
+
+
+cd packages_services_Telephony
+git push origin oreo backup --force
+cd ..
+
+
+cd system_bt
+git push origin oreo backup --force
+cd ..
+
+
+cd system_core
+git push origin oreo backup --force
+cd ..
+
+
+cd system_media
+git push origin oreo backup --force
+cd ..
+
+
+cd system_sepolicy
+git push origin oreo backup --force
+cd ..
+
+
+cd system_vold
+git push origin oreo backup --force
+cd ..
